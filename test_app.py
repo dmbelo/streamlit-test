7a1015052a084a60281f2a29773ec7e0d55c3baf
David Belo <dbelo@zoox.com>
2023-06-03 15:25:59 -0700

    Cleanup

--- test_app.py
+++ test_app.py
@@ -7,11 +7,7 @@ st.title("Fruit App")
 fruits = ["Apple", "Orange", "Mango", "Banana"]
 fruits_selection =  st.multiselect("Select your favorite fruits", fruits, [])
 
-if fruits_selection:
-    st.markdown("## Your Selection")
-    st.write(fruits_selection)
-
 if st.button('Make a smoothie'):
-    with st.spinner("coming right up..."):
+    with st.spinner("Coming right up..."):
         time.sleep(2)
-        st.write("Tadaaaaa! Here's your" + "+ ".join(fruits_selection) + " smoothie")
+        st.write("Tadaaaaa! Here's your " + "+ ".join(fruits_selection) + " smoothie")
